2c6344104e24937a03027822ad580c80e1333d76
mafions <miralion2707@gmail.com>
2024-06-06 18:56:54 +0300

    New goals

--- src/pages/Topurarlar.jsx
+++ src/pages/Topurarlar.jsx
@@ -4,13 +4,14 @@ import PlayerList from '../components/PlayerList';
 function Topurarlar() {
     const [players, setPlayers] = useState([
         { id: 8, name: 'Abrorbek (1995)', age: "?", position: 'Forward', goals: 14, reputation: "" },
+        { id: 4, name: 'Javoxir (2007)', age: 17, position: 'Forward', goals: 10, reputation: "" },
         { id: 7, name: 'Diyorbek', age: 20, position: 'Forward', goals: 7, reputation: "" },
         { id: 1, name: 'Shoxrux (2006)', age: 18, position: 'Forward', goals: 8, reputation: "" },
         { id: 2, name: 'Rasulbek', age: 19, position: 'Forward', goals: 8, reputation: "" },
         { id: 9, name: 'Xayotbek', age: 20, position: 'Forward', goals: 11, reputation: "" },
         { id: 4, name: 'Izzatbek (1996)', age: "?", position: 'Forward', goals: 6, reputation: "" },
-        { id: 4, name: 'Javoxir (2007)', age: 17, position: 'Forward', goals: 6, reputation: "" },
         { id: 7, name: 'Abu', age: 20, position: 'Defender', goals: 6, reputation: "" },
+        { id: 16, name: 'Oqilbek (2005)', age: "19", position: 'Forward', goals: 5, reputation: "" },
         { id: 14, name: 'Mirzadavlat', age: 17, position: 'Midfielder', goals: 5, reputation: "" },
         { id: 3, name: 'Ismoil (2006) ', age: 18, position: 'Midfielder', goals: 3, reputation: "" },
         { id: 7, name: 'Nodirbek', age: 18, position: 'Defender', goals: 2, reputation: "" },
@@ -22,6 +23,7 @@ function Topurarlar() {
         { id: 13, name: 'Akmal', age: 17, position: 'Forward', goals: 1, reputation: "" },
         { id: 14, name: 'Ulugbek (1995)', age: "?", position: 'Forward', goals: 1, reputation: "" },
         { id: 15, name: 'Nuriddin (1995)', age: "?", position: 'Forward', goals: 1, reputation: "" },
+
     ]);
 
     return (
